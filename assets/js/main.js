d6adfefce60c03af3c81f08454fea4f214a8b084
Your Name <musepsycho98@gmail.com>
2018-10-11 21:10:36 +0400

    first

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+
+var mainText = document.getElementById('paragraph').innerHTML;
+
+ function highlight () {
+    var input = document.getElementById('typed-text').value;
+
+    if (1) {
+        document.getElementById('paragraph').innerHTML = mainText.split(input).join('<span style="background-color: #ffff00;">'+input+'</span>');
+    }
+}
+
+
+
