d3ff682f4d58b2f056882b504b41791d32bb4274
Your Name <musepsycho98@gmail.com>
2018-10-12 22:26:50 +0400

    added one button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
 
-
-
 var mainText = document.getElementById('paragraph').textContent;
+var toLower = mainText.toLowerCase();
 
- function highlight () {
+function highlight() {
     var input = document.getElementById('typed-text').value;
-
-    if (1) {
-        document.getElementById('paragraph').innerHTML = mainText.split(input).join('<span style="background-color: #ffff00;">'+input+'</span>');
-    }
+    document.getElementById('paragraph').innerHTML = mainText.replace(input, '<span style="background-color: #ffff00;">' + input + '</span>');
 }
 
+function highlightAll() {
+    var input = document.getElementById('typed-text').value;
+    document.getElementById('paragraph').innerHTML = mainText.replace(new RegExp(input, 'g' ),'<span style="background-color: #ffff00;">' + input + '</span>');
+}
+console.log(toLower);
 
 
